9fa2e34950f4b98b29ce31a8bfdfbe613a13fd4c
David Wicks <wicks.db@gmail.com>
2013-09-07 18:29:35 -0400

    Updated script for copying entityx sources.

--- scripts/copy_entityx.sh
+++
@@ -1,4 +0,0 @@
-print "Will copy entityx files"
-rsync -arvh --progress --exclude "*_test.cc" --exclude "python" \
---exclude "Manager.*" \
-"/Users/davidwicks/Code/entityx/entityx/" "../src/entityx"
