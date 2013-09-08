4b943d9661d020207422b7d26dcd250bd8733c07
David Wicks <wicks.db@gmail.com>
2013-09-08 11:45:12 -0400

    Bug fix from entityx.

--- src/entityx/Entity.h
+++ src/entityx/Entity.h
@@ -291,8 +291,8 @@ class EntityManager : boost::noncopyable, public enable_shared_from_this<EntityM
       friend class View;
 
       Iterator(ptr<EntityManager> manager,
-               std::vector<Predicate> predicates,
-               std::vector<ptr<BaseUnpacker>> unpackers,
+               const std::vector<Predicate> &predicates,
+               const std::vector<ptr<BaseUnpacker>> &unpackers,
                uint32_t index)
           : manager_(manager), predicates_(predicates), unpackers_(unpackers), i_(index), capacity_(manager_->capacity()) {
         next();
@@ -302,6 +302,7 @@ class EntityManager : boost::noncopyable, public enable_shared_from_this<EntityM
         while (i_ < capacity_ && !predicate()) {
           ++i_;
         }
+
         if (i_ < capacity_ && !unpackers_.empty()) {
           Entity::Id id = manager_->create_id(i_);
           for (auto &unpacker : unpackers_) {
@@ -333,9 +334,9 @@ class EntityManager : boost::noncopyable, public enable_shared_from_this<EntityM
     }
 
     Iterator begin() { return Iterator(manager_, predicates_, unpackers_, 0); }
-    Iterator end() { return Iterator(manager_, predicates_, unpackers_, manager_->size()); }
+    Iterator end() { return Iterator(manager_, predicates_, unpackers_, manager_->capacity()); }
     const Iterator begin() const { return Iterator(manager_, predicates_, unpackers_, 0); }
-    const Iterator end() const { return Iterator(manager_, predicates_, unpackers_, manager_->size()); }
+    const Iterator end() const { return Iterator(manager_, predicates_, unpackers_, manager_->capacity()); }
 
     template <typename A>
     View &unpack_to(ptr<A> &a) {
